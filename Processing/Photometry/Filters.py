05ae6d16915b9ee778cfca894f6bd81f226c4c22
Daniel <dpalmer9@uwo.ca>
2022-07-12 13:52:23 -0400

    Added Filter Functions for Photometry

---
+++ Processing/Photometry/Filters.py
@@ -0,0 +1,35 @@
+# Import
+from scipy import signal
+
+
+def butterworth(time_data, data_iso, data_act, order, filter_freq, filt_type='lowpass', analog=False):
+    sample_freq = len(time_data) / (time_data[(len(time_data) - 1)] - time_data[0])
+    butter_filter = signal.butter(N=order, Wn=filter_freq, btype=filt_type, analog=analog,
+                                  output='sos', fs=sample_freq)
+    fil_iso = signal.sosfilt(butter_filter, data_iso)
+    fil_act = signal.sosfilt(butter_filter, data_act)
+    return fil_iso, fil_act
+
+def chebychev_i(time_data, data_iso, data_act, order, ripple, filter_freq, filt_type='lowpass', analog=False):
+    sample_freq = len(time_data) / (time_data[(len(time_data) - 1)] - time_data[0])
+    chebyi_filter = signal.cheby1(N=order, rp=ripple, Wn=filter_freq, btype=filt_type, analog=analog,
+                                  output='sos', fs=sample_freq)
+    fil_iso = signal.sosfilt(chebyi_filter, data_iso)
+    fil_act = signal.sosfilt(chebyi_filter, data_act)
+    return fil_iso, fil_act
+
+def chebychev_ii(time_data, data_iso, data_act, order, attenuation, filter_freq, filt_type='lowpass', analog=False):
+    sample_freq = len(time_data) / (time_data[(len(time_data) - 1)] - time_data[0])
+    chebyii_filter = signal.cheby1(N=order, rs=attenuation, Wn=filter_freq, btype=filt_type, analog=analog,
+                                  output='sos', fs=sample_freq)
+    fil_iso = signal.sosfilt(chebyii_filter, data_iso)
+    fil_act = signal.sosfilt(chebyii_filter, data_act)
+    return fil_iso, fil_act
+
+def bessel(time_data, data_iso, data_act, order, filter_freq, filt_type='lowpass', analog=False):
+    sample_freq = len(time_data) / (time_data[(len(time_data) - 1)] - time_data[0])
+    butter_filter = signal.bessel(N=order, Wn=filter_freq, btype=filt_type, analog=analog,
+                                  output='sos', fs=sample_freq)
+    fil_iso = signal.sosfilt(butter_filter, data_iso)
+    fil_act = signal.sosfilt(butter_filter, data_act)
+    return fil_iso, fil_act
