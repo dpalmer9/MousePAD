f9e1a67ec8454f76ba93bddfa412a8e9291e14c0
Daniel <dpalmer9@uwo.ca>
2022-07-12 15:46:31 -0400

    Updated Photometry Filter Functions

--- Processing/Photometry/Filters.py
+++ Processing/Photometry/Filters.py
@@ -10,6 +10,7 @@ def butterworth(time_data, data_iso, data_act, order, filter_freq, filt_type='lo
     fil_act = signal.sosfilt(butter_filter, data_act)
     return fil_iso, fil_act
 
+
 def chebychev_i(time_data, data_iso, data_act, order, ripple, filter_freq, filt_type='lowpass', analog=False):
     sample_freq = len(time_data) / (time_data[(len(time_data) - 1)] - time_data[0])
     chebyi_filter = signal.cheby1(N=order, rp=ripple, Wn=filter_freq, btype=filt_type, analog=analog,
@@ -18,18 +19,29 @@ def chebychev_i(time_data, data_iso, data_act, order, ripple, filter_freq, filt_
     fil_act = signal.sosfilt(chebyi_filter, data_act)
     return fil_iso, fil_act
 
+
 def chebychev_ii(time_data, data_iso, data_act, order, attenuation, filter_freq, filt_type='lowpass', analog=False):
     sample_freq = len(time_data) / (time_data[(len(time_data) - 1)] - time_data[0])
-    chebyii_filter = signal.cheby1(N=order, rs=attenuation, Wn=filter_freq, btype=filt_type, analog=analog,
-                                  output='sos', fs=sample_freq)
+    chebyii_filter = signal.cheby2(N=order, rs=attenuation, Wn=filter_freq, btype=filt_type, analog=analog,
+                                   output='sos', fs=sample_freq)
     fil_iso = signal.sosfilt(chebyii_filter, data_iso)
     fil_act = signal.sosfilt(chebyii_filter, data_act)
     return fil_iso, fil_act
 
+
 def bessel(time_data, data_iso, data_act, order, filter_freq, filt_type='lowpass', analog=False):
     sample_freq = len(time_data) / (time_data[(len(time_data) - 1)] - time_data[0])
-    butter_filter = signal.bessel(N=order, Wn=filter_freq, btype=filt_type, analog=analog,
+    bessel_filter = signal.bessel(N=order, Wn=filter_freq, btype=filt_type, analog=analog,
                                   output='sos', fs=sample_freq)
-    fil_iso = signal.sosfilt(butter_filter, data_iso)
-    fil_act = signal.sosfilt(butter_filter, data_act)
+    fil_iso = signal.sosfilt(bessel_filter, data_iso)
+    fil_act = signal.sosfilt(bessel_filter, data_act)
+    return fil_iso, fil_act
+
+
+def elliptic(time_data, data_iso, data_act, order, ripple, attenuation, filter_freq, filt_type='lowpass', analog=False):
+    sample_freq = len(time_data) / (time_data[(len(time_data) - 1)] - time_data[0])
+    ellip_filter = signal.ellip(N=order, rp=ripple, rs=attenuation, Wn=filter_freq, btype=filt_type, analog=analog,
+                                output='sos', fs=sample_freq)
+    fil_iso = signal.sosfilt(ellip_filter, data_iso)
+    fil_act = signal.sosfilt(ellip_filter, data_act)
     return fil_iso, fil_act
